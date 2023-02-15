41da45fb141377e5502a015599944f8ea3a620b8
unknown <12cmorrison12@gmail.com>
2023-02-14 18:45:00 -0700

    added in docstrings

--- serial.py
+++ serial.py
@@ -20,16 +20,18 @@ class SerialGenerator:
     100
     """
     def __init__(self, start=0):
+        """Makes a new serial generator, starting at the start input."""
         self.start = self.next = start
 
     def __repr__(self):
+        """Shows the representation logically."""
         return f"SerialGenerator(start={self.start} next={self.next})"
 
     def generate_num(self):
-
+        """Returns the next serial number."""
         self.next += 1
         return self.next - 1
 
     def reset(self):
-
+        """Resets the generator to the original start number."""
         self.next = self.start
